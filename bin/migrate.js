8649d6c0c8cddbd74c6bdf0ebb11b89a4d5289a3
thomas.cocchiara <thomas.cocchiara@gmail.com>
2015-07-23 13:40:23 +0200

    removed environment dependencies and transformed the file .migrate.json into .migrate.js. With this modification we can now use variables instead of strings

--- bin/migrate.js
+++ bin/migrate.js
@@ -7,12 +7,14 @@ var slug = require('slug');
 var path = require('path');
 var fs = require('fs');
 
+var config_path = process.cwd() + '/.migrate.js';
+
 var CONFIG;
 
 // The models should be shared between migration files
+var timestamp_path = process.cwd() + '/.timestamp.json';
 
-var models_config_path = process.cwd() + '/.migrate-models.json';
-var MODELS_CONFIG;
+var timestamp_CONFIG;
 
 program
   .command('init')
@@ -36,11 +38,7 @@ program
 
 program.version(require('../package.json').version);
 
-// Specify an environment appending -e <environment_name> or --environment <environment_name> to the command
-
-program
-  .option('-e, --environment <name>', 'add the environment')
-  .parse(process.argv);
+program.parse(process.argv);
   
 
 // Default command ?
@@ -63,53 +61,31 @@ function success(msg) {
 
 function loadConfiguration() {
   try {
-    console.log(getConfigFilePath());
-    return require(getConfigFilePath());
+    return require(config_path);
   } catch (e) {
-    if (program.environment) {
-      error('Missing ' + getConfigFileName() + ' file. Type `migrate init --environment ' + program.environment + '` to create.');
+      error('Missing ' + config_path + ' file. Type `migrate init` to create.');
     }
-    else {
-      error('Missing ' + getConfigFileName() + ' file. Type `migrate init` to create.');
-    }
-  }
 }
 
-function loadModelsConfiguration() {
+function timestampConfiguration() {
   try {
-    return require(models_config_path);
+    return require(timestamp_path);
   } catch (e) {
-    if (program.environment) {
-      error('Missing .migrate-models.json file. Type `migrate init --environment ' + program.environment + '` to create.');
-    }
-    else {
-      error('Missing .migrate-models.json file. Type `migrate init` to create.');
-    }
+    var data = JSON.stringify({ current_timestamp: 0 }, null, 2);
+      fs.writeFileSync(timestamp_path, data);
   }
 }
 
 function updateTimestamp(timestamp, cb) {
-  CONFIG.current_timestamp = timestamp;
-  var data = JSON.stringify(CONFIG, null, 2);
-  fs.writeFile(getConfigFilePath(), data, cb);
+  timestamp_CONFIG.current_timestamp = timestamp;
+  var data = JSON.stringify(timestamp_CONFIG, null, 2);
+  fs.writeFile(timestamp_path, data, cb);
 }
 
 function init() {
   
-  if (!fs.existsSync(models_config_path)) {
-    MODELS_CONFIG = {
-      models: {}
-    };
-    
-    var data = JSON.stringify(MODELS_CONFIG, null, 2);
-    console.log(models_config_path);
-    fs.writeFileSync(models_config_path, data);
-
-    success(models_config_path + ' file created!\nEdit it to include your models definitions');
-  }
-  
-  if (fs.existsSync(getConfigFilePath())) {
-    error(getConfigFileName() + ' already exists!');
+  if (fs.existsSync(config_path)) {
+    error(config_path + ' already exists!');
   }
 
   var schema = {
@@ -129,17 +105,15 @@ function init() {
 
   prompt.start();
   prompt.get(schema, function (error, result) {
-    CONFIG = {
-      basepath: result.basepath,
-      connection: result.connection,
-      current_timestamp: 0
-    };
 
-    var data = JSON.stringify(CONFIG, null, 2);
+    var data = fs.readFileSync(path.normalize(__dirname + '/../template/config.js'), 'ascii');
+    data = data
+      .replace('MIGRATION_KEY', result.basepath)
+      .replace('CONNECTION_KEY', result.connection);
     
-    fs.writeFileSync(getConfigFilePath(), data);
+    fs.writeFileSync(config_path, data);
 
-    success(getConfigFileName() + ' file created!\n');
+    success(config_path + ' file created!\nEdit to add your models');
     process.exit();
   });
 }
@@ -147,7 +121,7 @@ function init() {
 function createMigration(description) {
 
   CONFIG = loadConfiguration();
-  CONFIG.models = loadModelsConfiguration().models;
+  timestamp_CONFIG = timestampConfiguration();
 
   var timestamp = Date.now();
   var migrationName = timestamp + '-' + slug(description) + '.js';
@@ -173,7 +147,6 @@ function connnectDB() {
 }
 
 function loadModel(model_name) {
-  
   return require(process.cwd() + '/' + CONFIG.models[model_name]);
 }
 
@@ -184,7 +157,7 @@ function getTimestamp(name) {
 function migrate(direction, cb, number_of_migrations) {
 
   CONFIG = loadConfiguration();
-  CONFIG.models = loadModelsConfiguration().models;
+  timestamp_CONFIG = timestampConfiguration();
   
   if (!number_of_migrations) {
     number_of_migrations = 1;
@@ -202,9 +175,9 @@ function migrate(direction, cb, number_of_migrations) {
     var timestamp = getTimestamp(migration_name);
 
     if (number_of_migrations > 0) {
-      return timestamp > CONFIG.current_timestamp;
+      return timestamp > timestamp_CONFIG.current_timestamp;
     } else if (number_of_migrations < 0) {
-      return timestamp <= CONFIG.current_timestamp;
+      return timestamp <= timestamp_CONFIG.current_timestamp;
     }
   });
 
@@ -247,15 +220,4 @@ function applyMigration(direction, name, cb) {
 
     updateTimestamp(timestamp, cb);
   }
-}
-
-function getConfigFileName() {
-  if (program.environment) {
-    return '.migrate' + '-' + program.environment + '.json';
-  }
-  return '.migrate.json';
-}
-
-function getConfigFilePath() {
-  return process.cwd() + '/' + getConfigFileName();
 }
